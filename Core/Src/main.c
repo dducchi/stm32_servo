4561657ff08149de35222ed756822f3961e3b23e
dducchi <dducchi@kookmin.ac.kr>
2023-02-09 13:25:40 +0900

    6class_

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -40,6 +40,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim3;
 
 /* USER CODE BEGIN PV */
 
@@ -49,19 +50,25 @@ TIM_HandleTypeDef htim1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#define OneRotatioPulses 20
+uint8_t cnt1, cnt2, diff, speed, dir;
+uint32_t tick;
+
+
 long map(long x, long in_min, long in_max, long out_min, long out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void setServo(int inData){
-	htim1.Instance->CCR1 = map(inData, 0, 100, 999, 1999); //1000~2000 을 줘야하는데 들어오는 값은 0~100
-	//180도 움직이게 하고 싶으면 999>700, 1999>2500
+	htim1.Instance->CCR1 = map(inData, 0, 100, 700, 2500); //1000~2000 �?� 줘야하는�?� 들어오는 값�?� 0~100
+	//180�?� 움�?�?�게 하고 싶으면 999>700, 1999>2500
 }
 /* USER CODE END 0 */
 
@@ -94,8 +101,19 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM1_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+
+HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
+
+/*HAL_TIM_Encoder_Start(&htim3,TIM_CHANNEL_ALL);
+HAL_TIM_OC_Start(&htim4, TIM_CHANNEL_1);
+HAL_TIM_OC_Start(&htim4, TIM_CHANNEL_2);
+
+cnt1 = TIM3->CNT;
+tick = HAL_GetTick();*/
+//시스틱 사용할 때 hamdler�?서 hal_intick 으로 �?�해서 전�?�?� 켜지면 무한으로 1ms마다 1씩 �?가함
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -105,7 +123,9 @@ HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  for(int i = 0; i <= 100; i++){
+	  setServo(htim3.Instance->CNT);
+	  HAL_Delay(50);
+	  /*for(int i = 0; i <= 100; i++){
 		  setServo(i);
 		  HAL_Delay(10);
 	  }
@@ -113,7 +133,47 @@ HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
 		  setServo(i);
 		  HAL_Delay(10);
 	  }
-	  //왼쪽 오른쪽 왔다 갓다 하게끔 만들기
+	  //왼쪽 오른쪽 왔다 갓다 하게�?� 만들기*/
+	  /* if(HAL_GetTick() - tick > 1000L) {
+		  //오랜시간�?� 걸리면 L�?� 붙여주면 �?�
+		  //1초�? 한번씩 �?� 안으로 들어오겠다는 �?임
+		  cnt2 = TIM3->CNT; //1초�?�안 얼만�?� �?가 혹�?� �?소했는지 값�?� 확�?�할 수 있�?�
+
+		  //chec direction
+		  if(__HAL_TIM_IS_TIM_COUNTING_DOWN(&htim3)){9
+			  dir - 1;
+			  if((cnt2 > cnt1) && (cnt2 - cnt1 < 100))dir = 0;
+		  }
+		  else {
+			  dir = 0; //up
+			  if((cnt1 > cnt2) && (cnt1 - cnt2 < 100)) dir = 1;
+		  }
+
+		  if (dir){
+			  if(cnt1 >= cnt2) diff = cnt1 - cnt2;
+			  else diff = (TIM3->ARR + cnt1) - cnt2;
+			  //ARR = 65535;
+		  }
+		  else {
+			  if(cnt2 >= cnt1) diff = cnt2 - cnt1;
+			  else diff = (TIM3->ARR + cnt2) - cnt1;
+		  }// 방향�?� 어�?방향�?�지
+
+		  //회전 �?�?�가 얼마나 빠른지 체�?�
+		  if((TIM3->SMCR & 0x03) == 0x03){
+			  speed = diff * 60 / 4 / OneRotatioPulses;
+		  }
+		  else {
+			  speed = diff * 60 / 2 / OneRotatioPulses;
+		  }
+
+		  tick = HAL_GetTick(); //tick�?� 다�?��?� 위해서
+		  cnt1 = TIM3->CNT;
+
+		  setServo(map(cnt1, 0, 65535, 0, 100));
+	  }//main함수가 닫힘
+
+  }//main if가 닫힘 */
   }
   /* USER CODE END 3 */
 }
@@ -182,7 +242,7 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 15;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 65535;
+  htim1.Init.Period = 20000-1;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -231,6 +291,55 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 2 */
   HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_Encoder_InitTypeDef sConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 0;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 100;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
+  sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
+  sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
+  sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
+  sConfig.IC1Filter = 0;
+  sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
+  sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
+  sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
+  sConfig.IC2Filter = 0;
+  if (HAL_TIM_Encoder_Init(&htim3, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
 
 }
 
